2bca4737b45ef9b0c372cb62d77024f982410709
sbfqusb <sbfqusb@163.com>
2017-10-24 09:38:05 +0800

    modfy tsdk

--- src/battle/Player.ts
+++ src/battle/Player.ts
@@ -1,14 +1,14 @@
 /*
 * name;
 */
-class Player{
-    constructor(){
+class Player {
+    constructor() {
 
     }
-    private property:Object;
-    public x:number = 0;
-    public y:number = 0;
-    public mIsDestroy:boolean = false;//对象摧毁
-    public CastSkill(skillId:number):void{}
-    public tick(dt){}
+    private property: Object;
+    public x: number = 0;
+    public y: number = 0;
+    public mIsDestroy: boolean = false;//对象摧毁
+    public CastSkill(skillId: number): void { }
+    public tick(dt) { }
 }
